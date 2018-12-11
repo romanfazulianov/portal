f271d67aa688203c1389fa5002c2dd818f787956
Roman Fazulianov <romanfazulianov@users.noreply.github.com>
2018-12-11 11:08:50 +0400

    update Draggable

--- src/Draggable.js
+++ src/Draggable.js
@@ -4,7 +4,7 @@ import Draggable from 'react-draggable';
 import {Portal} from './Portal';
 
 export class DraggablePortal extends Portal {
-  renderPortal({children, ...props}) {
-    ReactDOM.render(this.portal, <Draggable {...props}>{children}</Draggable>);
+  renderer(props, children) {
+    return <Draggable {...props}>{children}</Draggable>;
   }
 }
